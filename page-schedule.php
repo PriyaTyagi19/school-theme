39a11cc18973f479927b75328c9e1897d61f9c01
tylerjwhitman <156140229+tylerjwhitman@users.noreply.github.com>
2024-06-05 15:33:30 -0700

    Repeater ACF added

---
+++ page-schedule.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package School_Theme
+ */
+
+get_header(); ?>
+
+<main id="primary" class="site-main">
+
+    <?php
+    while ( have_posts() ) :
+        the_post();
+        ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+            </header>
+
+            <div class="entry-content">
+                <?php
+                the_content();
+
+                // https://www.advancedcustomfields.com/resources/repeater/
+                if( have_rows('schedule') ): ?>
+                    <h2>Weekly Course Schedule</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Course</th>
+                                <th>Instructor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        <?php
+                        while( have_rows('schedule') ) : the_row(); ?>
+                            <tr>
+                            <!-- Display subfields -->
+                            <td><?php the_sub_field('date'); ?></td>
+                            <td><?php the_sub_field('course'); ?></td>
+                            <td><?php the_sub_field('instructor'); ?></td>
+                            </tr>
+
+                        <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+            </div>
+
+            <?php
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+            ?>
+        </article> <?php the_ID(); ?>
+
+    <?php
+    endwhile; // End of the loop.
+    ?>
+
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
